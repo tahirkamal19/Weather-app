045d3c2eb3c1d5164eac29bf9fcb7ec789b5b008
Tahir Kamal <tahirkamal6@gmail.com>
2024-10-28 23:50:03 +0500

    commited few css changes and added fields

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
   const [city, setCity] = useState('');
   return (
     <>
-      <h1 className="text-3xl font-light  text-white">Weather Up!</h1>
+    <div className="bg-blue-700 border-2 border-blue-950 rounded-xl w-64 m-auto mb-16 h-20">
+      <h1 className="text-3xl font-light mt-5 text-white">Weather Up!</h1>
+    </div>
       <Searchbar setCity={setCity}/>
       <Main city={city}/>
     </>
--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -2,39 +2,76 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export const Main = ({city}) => {
-    const [weather, setWeather] = useState(null)
+export const Main = ({ city }) => {
+  const [weather, setWeather] = useState(null);
 
-    const apiKey='b920e03419610a8aecf2529f7eb65e56'
-    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  const apiKey = "b920e03419610a8aecf2529f7eb65e56";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-    const getWeather =async()=>{
-        try {
-            const res = await axios.get(url)
-            setWeather(res.data)
-            console.log(res.data)
-        } catch (error) {
-            console.error(error)
-        }
+  const getWeather = async () => {
+    try {
+      const res = await axios.get(url);
+      setWeather(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
     }
-    useEffect(() => {
-      getWeather()
-    }, [city])
-    
+  };
+  useEffect(() => {
+    getWeather();
+  }, [city]);
 
   return (
-    <div className="border-2 bg-blue-800 my-10 border-blue-950 rounded-2xl">
-      <div className="my-8 bg- border-2  border-blue-950  w-fit p-7 rounded-3xl mx-auto">
-        <h1 className="font-bold text-blue-400 text-3xl">{city}</h1>
-      </div>
-      <div className="my-20 flex flex-row gap-9 justify-center">
-        <div className="rounded-3xl border-2 border-blue-950 p-12">
-        <h1>{weather?.main?.temp}<sup>o</sup>C</h1>
+    <div>
+      {weather ? (
+        <div className="border-2 bg-blue-800 my-10 border-blue-950 rounded-2xl">
+          <div className="my-8 bg- border-2  border-blue-950  w-fit p-7 rounded-3xl mx-auto">
+            <h1 className="font-bold text-blue-50 text-3xl">{weather?.name}</h1>
+          </div>
+
+          <hr />
+
+        <div className="flex flex-row items-baseline justify-center gap-5">
+          <div className="  mt-5 w-fit rounded-3xl border-2 border-blue-950 h-fit p-12">
+            <h1 className="text-blue-50 text-3xl">
+              Temprature:{<br/>}
+              {weather?.main?.temp}
+              <sup>o</sup>C
+            </h1>
+          </div>
+
+            <div className="border-2 w-fit rounded-3xl border-blue-950 p-12">
+              <h1 className="text-blue-50 text-3xl">{weather?.weather[0]?.description}</h1>
+              <div className="w-20 h-20 m-auto bg-black rounded-3xl">
+                <img
+                  src={`http://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`}
+                  alt=""
+                />
+              </div>
+            </div>
         </div>
-        <div className="border-2 rounded-3xl border-blue-950 p-12">
-    <h1>{weather?.weather[0]?.description}</h1>
+          <div className="my-20 flex flex-row gap-9 justify-center">
+
+            <div className=" rounded-3xl border-2 border-blue-950 h-fit p-12">
+              <h1 className="text-blue-50 text-3xl">
+                Feels Like:{<br />} {weather?.main?.feels_like}
+                <sup>o</sup>C
+              </h1>
+            </div>
+
+            <div className="rounded-3xl border-2 border-blue-950 h-fit p-12">
+              <h1 className="text-blue-50 text-3xl">Humidity: {weather?.main?.humidity}%</h1>
+            </div>
+
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="w-64 m-auto h-24 border-2 bg-blue-800 my-10 border-blue-950 rounded-2xl">
+          <h1 className="mt-5 text-xl text-white">
+            Search a City to find it's Weather!
+          </h1>
+        </div>
+      )}
     </div>
   );
 };
